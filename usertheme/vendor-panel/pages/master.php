5536500cf813bc8f8375649cfbf0fc4f8ff08706
ketansuthar3110 <ketansuthar999@gmail.com>
2022-05-05 01:18:33 +0530

    5/05/2022 Final Website

--- usertheme/vendor-panel/pages/master.php
+++ usertheme/vendor-panel/pages/master.php
@@ -45,7 +45,7 @@ if ($_POST) {
         <link rel="stylesheet" href="../assets/vendor/fonts/material-design-iconic-font/css/materialdesignicons.min.css">
         <link rel="stylesheet" href="../assets/vendor/charts/c3charts/c3.css">
         <link rel="stylesheet" href="../assets/vendor/fonts/flag-icon-css/flag-icon.min.css">
-        <title>Concept - Bootstrap 4 Admin Dashboard Template</title>
+        <title>Vendor Dashboard </title>
     </head>
 
     <body>
@@ -65,9 +65,40 @@ if ($_POST) {
             <!-- ============================================================== -->
             <!-- left sidebar -->
             <!-- ============================================================== -->
-            <?php
-            include './theme/sidebar.php';
-            ?>
+           <div class="nav-left-sidebar sidebar-dark">
+    <div class="menu-list">
+        <nav class="navbar navbar-expand-lg navbar-light">
+            <a class="d-xl-none d-lg-none" href="#">Dashboard</a>
+            <button class="navbar-toggler"type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav flex-column">
+
+
+
+
+
+
+
+
+
+                    <a href ="master.php" class="nav-link active" style="color: #ffffff;font-size: 18px;"><i class="fa fa-user db-icon"></i> Dashboard</a>
+                    <a href ="view-booking-vendor.php" class="nav-link"style="color: #ffffff;font-size: 18px;"><i class="fa fa-list db-icon"></i> View Booking</a>
+                    <a href ="view-review-vendorpanel.php" class="nav-link"style="color: #ffffff;font-size: 18px;"> <i class="fa fa-heart db-icon"></i>View User Review</a>
+                    <a href ="change-pass-vendor.php" class="nav-link"style="color: #ffffff;font-size: 18px;"><i class="fas fa-key"></i> Change Password</a>
+                    <a href ="vendor-report/vendor_booking_reportrpt.php" target="_blank" class="nav-link"style="color: #ffffff;font-size: 18px;"><i class="fas fa-chart-line"></i> Generate Report</a>
+
+                    <a href ="logout-vendor.php" class="nav-link"style="color: #ffffff;font-size: 18px;"><i class="fas fa-sign-out-alt"></i> Logout</a>
+
+
+
+
+                </ul>
+            </div>
+        </nav>
+    </div>
+</div>
             <!-- ============================================================== -->
             <!-- end left sidebar -->
             <!-- ============================================================== -->
--- usertheme/vendor-panel/pages/view-booking-vendor.php
+++ usertheme/vendor-panel/pages/view-booking-vendor.php
@@ -30,9 +30,40 @@ $dmsg = "";
             include './theme/header.php';
             ?>
 
-            <?php
-            include './theme/sidebar.php';
-            ?>
+           <div class="nav-left-sidebar sidebar-dark">
+    <div class="menu-list">
+        <nav class="navbar navbar-expand-lg navbar-light">
+            <a class="d-xl-none d-lg-none" href="#">Dashboard</a>
+            <button class="navbar-toggler"type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav flex-column">
+
+
+
+
+
+
+
+
+
+                    <a href ="master.php" class="nav-link" style="color: #ffffff;font-size: 18px;"><i class="fa fa-user db-icon"></i> Dashboard</a>
+                    <a href ="view-booking-vendor.php" class="nav-link active"style="color: #ffffff;font-size: 18px;"><i class="fa fa-list db-icon"></i> View Booking</a>
+                    <a href ="view-review-vendorpanel.php" class="nav-link"style="color: #ffffff;font-size: 18px;"> <i class="fa fa-heart db-icon"></i>View User Review</a>
+                    <a href ="change-pass-vendor.php" class="nav-link"style="color: #ffffff;font-size: 18px;"><i class="fas fa-key"></i> Change Password</a>
+                    <a href ="vendor-report/vendor_booking_reportrpt.php" target="_blank" class="nav-link"style="color: #ffffff;font-size: 18px;"><i class="fas fa-chart-line"></i> Generate Report</a>
+
+                    <a href ="logout-vendor.php" class="nav-link"style="color: #ffffff;font-size: 18px;"><i class="fas fa-sign-out-alt"></i> Logout</a>
+
+
+
+
+                </ul>
+            </div>
+        </nav>
+    </div>
+</div>
 
             <div class="dashboard-wrapper">
                 <div class="container-fluid  dashboard-content">
